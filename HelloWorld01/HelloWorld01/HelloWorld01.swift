43b383a2328c860772f7241bf6c63869efa90eaa
jay.lam <jay.lam@bbpos.com>
2022-04-19 15:37:44 +0800

    1.0.0

---
+++ HelloWorld01/HelloWorld01/HelloWorld01.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld01.swift
+//  HelloWorld01
+//
+//  Created by jay.lam on 19/4/2022.
+//
+
+public class HelloWorld01 {
+    public static func helloWorld(name: String) {
+        print("Hello World, \(name)")
+    }
+    
+    public static func bye(name: String) -> String {
+        return "Bye \(name)!!"
+    }
+}
